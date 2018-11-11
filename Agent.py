02659788cfc2d31ba7ee2739943bf498f6752fd9
newalter <zeyong1123@gmail.com>
2018-11-11 16:13:04 +0800

    basic qlearning

---
+++ Agent.py
@@ -0,0 +1,65 @@
+import random
+
+class QAgent():
+    actionSpace = None
+    totalMoves = 0
+    previousAction = None
+    previousObservation = None
+    previousReward = None
+    alpha = 0.5
+    gamma = 0.9
+    Q = {}
+    frequency = {}
+    def __init__(self, actionSpace):
+        self.actionSpace = actionSpace
+
+    def move(self, observation, reward, done):
+        if done:
+            self.Q[(observation[0],observation[1])] = reward
+        s_a = (self.previousObservation[0], self.previousObservation[1], self.previousAction)
+        self.frequency[s_a] = self.frequency.get(s_a, 0) + 1
+        this_q = self.Q.get(s_a,0)
+        self.Q[s_a] = this_q + self.alpha*(self.previousReward + self.gamma * self.max_lookup(observation) - this_q)
+        self.previousObservation = observation
+        self.previousReward = reward
+        action = self.get_action(observation)
+        self.previousAction = action
+        return action
+
+    def new_episode(self, observation):
+        action = self.get_action(observation)
+        self.previousAction = action
+        self.previousObservation = observation
+        self.previousReward = -1.0
+        return action
+
+    def get_action(self, observation):
+        self.totalMoves = self.totalMoves + 1
+        if random.random() < self.totalMoves / 1000000:
+            return self.max_arg(observation)
+        else:
+            return self.actionSpace.sample()
+
+
+    def max_lookup(self, observation):
+        pos = observation[0]
+        velocity = observation[1]
+        maxR = self.Q.get((pos,velocity), -1000)
+        maxR = max(maxR, self.Q.get((pos, velocity, 0), -1000))
+        maxR = max(maxR, self.Q.get((pos, velocity, 1), -1000))
+        maxR = max(maxR, self.Q.get((pos, velocity, 2), -1000))
+        return maxR
+
+    def max_arg(self, observation):
+        pos = observation[0]
+        velocity = observation[1]
+        action = 0
+        maxR = self.Q.get((pos, velocity, 0), -1000)
+        if maxR < self.Q.get((pos, velocity, 1), -1000):
+            maxR = self.Q.get((pos, velocity, 1), -1000)
+            action = 1
+        if maxR < self.Q.get((pos, velocity, 2), -1000):
+            maxR = self.Q.get((pos, velocity, 2), -1000)
+            action = 2
+        return action
+
---
+++ mountain.py
@@ -0,0 +1,26 @@
+import gym
+
+from Agent import QAgent
+
+EPISODES = 10000
+
+env = gym.make('MountainCar-v0')
+agent = QAgent(env.action_space)
+
+AvgReward = 0
+
+for episode in range(EPISODES):
+    totalReward = 0
+    observation = env.reset()
+    done = False
+    action = agent.new_episode(observation= observation)
+    while not done:
+        observation, reward, done, info = env.step(action)
+        # env.render()
+        totalReward += reward
+        action = agent.move(observation, reward, done)
+    print(totalReward)
+    AvgReward += totalReward
+
+print(AvgReward / EPISODES)
+env.close()
